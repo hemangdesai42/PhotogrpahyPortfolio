a7d324d02d78cb9b0c62e7b041a70deb4fad2ae6
Hemang Desai <hemangdesai@hemangs-mbp.myfiosgateway.com>
2022-08-23 15:15:37 -0400

    added views and downloads via api stats for unsplash

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,48 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Box from '@mui/material/Box';
-import BasicModal from "../components/modal";
+// import BasicModal from "../components/modal";
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
 
 const accessKey = process.env.UNSPLASH_ACCESS_KEY
 
 
 export const getStaticProps = async (context) => {
-  const photos = await fetch(`https://api.unsplash.com/users/he_mangd/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30`).then(r => r.json())
+  const photos = await fetch(`https://api.unsplash.com/users/he_mangd/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&stats=true`).then(r => r.json())
   return { props: { photos } }
 }
 
 export default function Album({ photos }) {
+  console.log(photos)
   return (
     <div>
-      <BasicModal />
     <Box className="box" sx={{ width: 800, overflowY: 'scroll', border: '1px solid grey' }}>
+    <ListSubheader component="div">My Photographs</ListSubheader>
     <ImageList variant="masonry" cols={3} gap={8}>
     {photos.map((photo) => (
     <ImageListItem key={photo.urls.raw}>
-      <img 
+      <img
         src={`${photo.urls.raw}?w=164&fit=crop&auto=format`}
         srcSet={`${photo.urls.raw}dpr=2 2x`}
         loading="lazy"
         sx={{ margin: 25}}
         // className="photo"
       />
+       <ImageListItemBar
+            title={"Downloads: " + photo.statistics.downloads.total}
+            subtitle={"Views: " + photo.statistics.views.total}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${photo.statistics.downloads.total}`}
+              >
+                <InfoIcon />
+              </IconButton>
+            }
+          />
     </ImageListItem>
       ))}
     </ImageList>
