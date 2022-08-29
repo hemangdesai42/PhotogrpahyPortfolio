8451ab5aab2bc877b5e86a486338bf64e3d0dcc2
Hemang Desai <hemangdesai@hemangs-mbp.myfiosgateway.com>
2022-08-29 15:39:45 -0400

    added persisting navbar with naviagtion to home and photographs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import ResponsiveAppBar from '../components/navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <ResponsiveAppBar/>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/art/photography.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Box from '@mui/material/Box';
+// import BasicModal from "../components/modal";
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+import Link from 'next/link';
+
+const accessKey = process.env.UNSPLASH_ACCESS_KEY
+
+
+export const getStaticProps = async (context) => {
+  const photos = await fetch(`https://api.unsplash.com/users/he_mangd/photos?client_id=${accessKey}&per_page=30&stats=true`).then(r => r.json())
+  return { props: { photos } }
+}
+
+export default function Album({ photos }) {
+  return (
+    <div>
+    <Box className="box" sx={{ width: 900, overflowY: 'scroll', border: '1px solid grey' }}>
+    <ListSubheader component="div">My Photographs</ListSubheader>
+    <ImageList variant="masonry" cols={3} gap={8}>
+    {photos.map((photo) => (
+    <ImageListItem className="imagelistitem" key={photo.urls.raw}>
+      <img
+        src={`${photo.urls.raw}?w=164&fit=crop&auto=format`}
+        srcSet={`${photo.urls.raw}dpr=0 2x`}
+        loading="lazy"
+        sx={{ margin: 40}}
+        // className="photo"
+      />
+       <ImageListItemBar
+            className='imagelistitembar'
+            loading="lazy"
+            title={"Downloads: " + photo.statistics.downloads.total}
+            subtitle={"Views: " + photo.statistics.views.total}
+            actionIcon={
+              <IconButton
+                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                aria-label={`info about ${photo.statistics.downloads.total}`}
+              >
+                {/* <BasicModal/> */}
+                <InfoIcon />
+              </IconButton>
+            }
+          />
+    </ImageListItem>
+      ))}
+    </ImageList>
+    </Box>
+    </div>
+  );
+}
