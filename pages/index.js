4c8a29b86af9f99576c18e1decd47c3a865acf8f
Hemang Desai <hemangdesai@hemangs-mbp.myfiosgateway.com>
2022-08-29 14:35:07 -0400

    removed modal and plan to make an onclick event for each image as well as some css

--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,29 @@ import InfoIcon from '@mui/icons-material/Info';
 const accessKey = process.env.UNSPLASH_ACCESS_KEY
 
 
-export const getServerSideProps = async (context) => {
-  const photos = await fetch(`https://api.unsplash.com/users/he_mangd/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&per_page=30&stats=true`).then(r => r.json())
+export const getStaticProps = async (context) => {
+  const photos = await fetch(`https://api.unsplash.com/users/he_mangd/photos?client_id=${accessKey}&per_page=30&stats=true`).then(r => r.json())
   return { props: { photos } }
 }
 
 export default function Album({ photos }) {
   return (
     <div>
-    <Box className="box" sx={{ width: 800, overflowY: 'scroll', border: '1px solid grey' }}>
+    <Box className="box" sx={{ width: 900, overflowY: 'scroll', border: '1px solid grey' }}>
     <ListSubheader component="div">My Photographs</ListSubheader>
     <ImageList variant="masonry" cols={3} gap={8}>
     {photos.map((photo) => (
     <ImageListItem className="imagelistitem" key={photo.urls.raw}>
       <img
         src={`${photo.urls.raw}?w=164&fit=crop&auto=format`}
-        srcSet={`${photo.urls.raw}dpr=2 2x`}
+        srcSet={`${photo.urls.raw}dpr=0 2x`}
         loading="lazy"
         sx={{ margin: 40}}
         // className="photo"
       />
        <ImageListItemBar
             className='imagelistitembar'
+            loading="lazy"
             title={"Downloads: " + photo.statistics.downloads.total}
             subtitle={"Views: " + photo.statistics.views.total}
             actionIcon={
@@ -40,7 +41,7 @@ export default function Album({ photos }) {
                 sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                 aria-label={`info about ${photo.statistics.downloads.total}`}
               >
-                <BasicModal/>
+                {/* <BasicModal/> */}
                 <InfoIcon />
               </IconButton>
             }
